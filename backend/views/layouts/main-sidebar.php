fa09389251b089c167973cb86679c43528d72229
ouqunfu <ouqunfu@foxmail.com>
2018-01-05 11:39:42 +0800

    侧边栏选中状态修改

--- backend/views/layouts/main-sidebar.php
+++ backend/views/layouts/main-sidebar.php
@@ -37,7 +37,7 @@ use yii\helpers\Url;
     <ul class="sidebar-menu" data-widget="tree">
       <li class="header">主导航</li>
       <!-- 首页 dashboard -->
-      <li class="active"><a href="<?php echo Url::toRoute('index/index');?>"><i class="fa fa-dashboard"></i> <span>首页</span></a></li>
+      <li <?php if('index/index' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('index/index');?>"><i class="fa fa-dashboard"></i> <span>首页</span></a></li>
       <!--内容-->
       <li class="treeview <?php if('contents' == $this->params['tag']) echo 'menu-open';?>">
         <a href="#"><i class="fa fa-list-alt"></i> <span>内容</span>
@@ -46,13 +46,13 @@ use yii\helpers\Url;
               </span>
         </a>
         <ul class="treeview-menu" <?php if('contents' == $this->params['tag']) echo 'style="display:block"';?>>
-          <li><a href="<?php echo Url::toRoute('columns/list');?>">栏目管理</a></li>
-          <li><a href="<?php echo Url::toRoute('cato/list');?>">分类管理</a></li>
-          <li><a href="<?php echo Url::toRoute('subjects/list');?>">专题管理</a></li>
-          <li><a href="<?php echo Url::toRoute('contents/list');?>">内容管理</a></li>
-          <li><a href="<?php echo Url::toRoute('url-manage/list');?>">URL规则</a></li>
-          <li><a href="<?php echo Url::toRoute('gallery/list-dir');?>">图片库</a></li>
-          <li><a href="<?php echo Url::toRoute('tags/list');?>">标签管理</a></li>
+          <li <?php if('columns/list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('columns/list');?>">栏目管理</a></li>
+          <li <?php if('cato/list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('cato/list');?>">分类管理</a></li>
+          <li <?php if('subject/list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('subjects/list');?>">专题管理</a></li>
+          <li <?php if('contents/list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('contents/list');?>">内容管理</a></li>
+          <li <?php if('url-manage/list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('url-manage/list');?>">URL规则</a></li>
+          <li <?php if('gallery/list-dir' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('gallery/list-dir');?>">图片库</a></li>
+          <li <?php if('tags/list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('tags/list');?>">标签管理</a></li>
         </ul>
       </li>
       <!--用户-->
@@ -75,10 +75,10 @@ use yii\helpers\Url;
               </span>
         </a>
         <ul class="treeview-menu" <?php if('functions' == $this->params['tag']) echo 'style="display:block"';?>>
-          <li><a href="<?php echo Url::toRoute('notice/list-notice');?>">公告管理</a></li>
-          <li><a href="<?php echo Url::toRoute('notice/list-message');?>">留言管理</a></li>
-          <li><a href="<?php echo Url::toRoute('comments/list');?>">评论管理</a></li>
-          <li><a href="<?php echo Url::toRoute('data-manage/index');?>">数据管理</a></li>
+          <li <?php if('notice/list-notice' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('notice/list-notice');?>">公告管理</a></li>
+          <li <?php if('notice/list-message' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('notice/list-message');?>">留言管理</a></li>
+          <li <?php if('comments/list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('comments/list');?>">评论管理</a></li>
+          <li <?php if('data-manage/index' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('data-manage/index');?>">数据管理</a></li>
         </ul>
       </li>
       <!--营销-->
@@ -89,8 +89,8 @@ use yii\helpers\Url;
               </span>
         </a>
         <ul class="treeview-menu" <?php if('market' == $this->params['tag']) echo 'style="display:block"';?>>
-          <li><a href="<?php echo Url::toRoute('market/list-seo-rule');?>">SEO管理</a></li>
-          <li><a href="<?php echo Url::toRoute('market/list-links');?>">友链管理</a></li>
+          <li <?php if('market/list-seo-rule' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('market/list-seo-rule');?>">SEO管理</a></li>
+          <li <?php if('market/list-links' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('market/list-links');?>">友链管理</a></li>
         </ul>
       </li>
       <!--设置-->
@@ -101,10 +101,10 @@ use yii\helpers\Url;
               </span>
         </a>
         <ul class="treeview-menu" <?php if('settings' == $this->params['tag']) echo 'style="display:block"';?>>
-          <li><a href="<?php echo Url::toRoute('settings/index');?>">网站配置</a></li>
-          <li><a href="<?php echo Url::toRoute('settings/watermark');?>">水印设置</a></li>
-          <li><a href="<?php echo Url::toRoute('settings/str-filter-list');?>">敏感词管理</a></li>
-          <li><a href="<?php echo Url::toRoute('settings/attachment');?>">附件设置</a></li>
+          <li <?php if('settings/index' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('settings/index');?>">网站配置</a></li>
+          <li <?php if('settings/watermark' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('settings/watermark');?>">水印设置</a></li>
+          <li <?php if('settings/str-filter-list' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('settings/str-filter-list');?>">敏感词管理</a></li>
+          <li <?php if('settings/attachment' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('settings/attachment');?>">附件设置</a></li>
         </ul>
       </li>
       <!--自定义-->
@@ -115,8 +115,8 @@ use yii\helpers\Url;
               </span>
         </a>
         <ul class="treeview-menu" <?php if('customs' == $this->params['tag']) echo 'style="display:block"';?>>
-          <li><a href="<?php echo Url::toRoute('customs/list-content-field');?>">内容字段</a></li>
-          <li><a href="<?php echo Url::toRoute('customs/list-user-field');?>">用户字段</a></li>
+          <li <?php if('customs/list-content-field' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('customs/list-content-field');?>">内容字段</a></li>
+          <li <?php if('customs/list-user-field' == $this->params['tag_li']) echo "class='active'"; ?>><a href="<?php echo Url::toRoute('customs/list-user-field');?>">用户字段</a></li>
         </ul>
       </li>
     </ul>
